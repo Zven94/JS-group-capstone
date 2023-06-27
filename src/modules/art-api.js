59c721d4a53e7509421a4a977ce895501570c2e2
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-06-27 18:42:45 +0200

    Create getArtWorks function to receive a promise

--- src/modules/art-api.js
+++ src/modules/art-api.js
@@ -2,3 +2,16 @@ const baseURL = 'https://api.artic.edu/api/v1/artworks';
 const pageNum = 'page=2';
 const limit = 'limit=12';
 const fullURL = `${baseURL}?${pageNum}&${limit}`;
+
+const getArtWorks = async () => {
+  try {
+    const result = await fetch(fullURL, { method: 'GET' });
+    const data = await result.json();
+    return data;
+  } catch (error) {
+    return {
+      Response: 'False',
+      Error: error.message || 'Unexpected error',
+    };
+  }
+};
