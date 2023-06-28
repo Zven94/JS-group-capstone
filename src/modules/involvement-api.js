bddeac9057c11134aa4a2b067fe3489773102913
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-06-28 17:27:25 +0200

    Add api unique id and url links

---
+++ src/modules/involvement-api.js
@@ -0,0 +1,4 @@
+const uniqueID = 'rTjcsvYJ35cWdaAtHbOK';
+const baseAPI = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps';
+const likesURL = `${baseAPI}/${uniqueID}/likes`;
+const commentsURL = `${baseAPI}/${uniqueID}/comments`;
