fa3cdc0501030df2d72711f8d67632e78c2c13cb
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-06-28 17:30:42 +0200

    Import functions to index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './style.css';
 import loadArtWorks from './modules/displayCards.js';
+import { loadLikes, sendLike } from './modules/involvement-api';
 
 window.addEventListener('DOMContentLoaded', loadArtWorks);
+window.addEventListener('DOMContentLoaded', loadLikes);
+// window.addEventListener('DOMContentLoaded', sendLike(1));
