099405e68f1105d96ccfe5d6bc2953f0475c3e8c
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-06-28 17:48:04 +0200

    Fix linters issue

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 import loadArtWorks from './modules/displayCards.js';
-import { loadLikes, sendLike } from './modules/involvement-api';
+import { loadLikes, sendLike } from './modules/involvement-api.js';
 
 window.addEventListener('DOMContentLoaded', loadArtWorks);
 window.addEventListener('DOMContentLoaded', loadLikes);
-// window.addEventListener('DOMContentLoaded', sendLike(1));
+window.addEventListener('DOMContentLoaded', sendLike(1));
