6d420f391ff7b1f88da9440ba7779c108263c2ec
Zven94 <nicobolanez@gmail.com>
2023-06-29 18:08:59 -0300

    "Added logo into the header, fix linters issues"

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import './style.css';
+import Logo from './logo.png';
 import loadArtWorks from './modules/displayCards.js';
+
+const myLogo = new Image();
+myLogo.src = Logo;
+const element = document.querySelector('.logo');
+
+element.appendChild(myLogo);
+const imgElement = element.querySelector('img');
+imgElement.classList.add('myLogo');
 
 window.addEventListener('DOMContentLoaded', loadArtWorks);
